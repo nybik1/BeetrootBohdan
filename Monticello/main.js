408308972f1f97b9ed8147387f03c5656c5a4fcd
nybik1 <spudegbutman@gmail.com>
2020-06-09 17:12:45 +0300

    add responsive// burger menu//fonts

--- Monticello/main.js
+++ Monticello/main.js
@@ -1,3 +1,16 @@
+////show&close menu
+$('.header__burger').click(function () {
+    $('.header__menu').toggleClass('header__menu-tooggler')
+});
+
+/// close menu when click on anchor
+let menu = document.querySelector('.header__menu');
+menu.addEventListener('click', (event) => {
+    menu.classList.add('header__menu-tooggler')
+});
+
+
+//slick slider
 $(document).ready(function () {
     $(".news__slider").slick({
         dots: true,
@@ -11,12 +24,21 @@ $(document).ready(function () {
         focusOnSelect: false,
         responsive: [
             {
-                breakpoint: 1350,
+                breakpoint: 1340,
+                settings: {
+                    dots: false,
+                }
+
+            },
+            {
+                breakpoint: 400,
                 settings: {
                     slidesToShow: 1,
                     dots: false,
                 }
+
             }
+
         ]
 
     });
@@ -28,7 +50,7 @@ $(document).ready(function () {
         $(".news__slider").slick("slickNext");
     })
 });
-
+//slick slider header
 $(document).ready(function () {
     $(".section1__slider").slick({
         slidesToShow: 1,
@@ -60,6 +82,7 @@ $(".header__menu a[href^=#], .section1__scrollDown a").on('click', function (eve
     $('html,body').animate({ scrollTop: $(target).offset().top }, 'slow');
 });
 
+//// reduce call time
 function handleScroll(selector, scrollTop) {
     const $selector = $(selector);
     const top = $selector.offset().top - 100;
@@ -89,7 +112,6 @@ $(document).on('scroll', debounce(function (e) {
     const scrollTop = $(this).scrollTop();
     if (scrollTop > 10) {
         $('.header').addClass('')
-        // header
     }
     handleScroll('#about', scrollTop);
     handleScroll('#projects', scrollTop);
@@ -99,14 +121,16 @@ $(document).on('scroll', debounce(function (e) {
 
 
 //// Sticky menu
-let menuPosition = $('.header').offset().top;
+let menuPosition = 1;
 $(window).scroll(function () {
     let menuTop = $(window).scrollTop();
     if (menuPosition < menuTop) {
         $('.header').addClass('sticky')
     }
     else { $('.header').removeClass('sticky') }
-})
+});
+
+
 
 
 
